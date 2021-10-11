871d63bd64fef5d2c25bbc2d7ba111dcd3f66880
siemah <hmi.dayenio@gmail.com>
2021-10-11 14:55:24 +0100

    feat:add post routes

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import {
 import routesEndpoints from "./config/routes/endpoints";
 import prisma from "./plugins/prisma";
 import apiKeysRoutes from "./routes/api-keys";
+import postsRoutes from "./routes/posts";
 import usersRoutes from "./routes/users";
 
 const app = createFastifyServer(fastifyFactoryOptions);
@@ -19,5 +20,6 @@ app.register(fastifyJWT, fastifyJWTOptions.plugin);
 app.register(prisma);
 app.register(usersRoutes, { prefix: routesEndpoints.auth.root });
 app.register(apiKeysRoutes, { prefix: routesEndpoints.apiKeys.root });
+app.register(postsRoutes, { prefix: routesEndpoints.posts.root });
 
 export default app;
